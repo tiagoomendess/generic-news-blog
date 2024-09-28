bb2df149fdee389404d543e431d01763ddd49326
Tiago Mendes <tiagoomendess@gmail.com>
2024-09-28 15:12:26 +0100

    fixed bug for older versions browsers

--- src/lib/stores/settings.ts
+++ src/lib/stores/settings.ts
@@ -2,19 +2,26 @@ import { get, writable, type Writable } from 'svelte/store';
 import { getSettings } from '$lib/api/getSettings';
 import type { Settings } from '$lib/utils/types';
 
+// Create a writable store for settings
 const settingsStore: Writable<Settings> = writable({} as Settings);
 
-const settings = await getSettings({ fetch });
-settingsStore.set(settings);
+// Create an async function to initialize the store with settings data
+const loadSettings = async () => {
+  const settings = await getSettings({ fetch });
+  settingsStore.set(settings);
+};
 
-export const fetchSettings = async () : Promise<Writable<Settings>> => {
-    const currentSettings = get(settingsStore);
-    if (!currentSettings.SiteName) {
-        const settings = await getSettings({ fetch });
-        settingsStore.set(settings);
-    }
+// Call the loadSettings function to fetch and set the initial settings
+loadSettings();
 
-    return settingsStore;
+export const fetchSettings = async (): Promise<Writable<Settings>> => {
+  const currentSettings = get(settingsStore);
+  if (!currentSettings.SiteName) {
+    const settings = await getSettings({ fetch });
+    settingsStore.set(settings);
+  }
+
+  return settingsStore;
 };
 
 export default settingsStore;
